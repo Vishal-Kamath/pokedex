a397285e73bea44f98387caf0035e23e8203c072
Vishal-Kamath <vishalkamath853@gmail.com>
2023-02-15 16:32:13 +0530

    sidenar design change

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,7 +17,7 @@ const Layout: React.FC<{ children: JSX.Element | JSX.Element[] }> = ({
       </Head>
       <div className="relative flex min-h-screen flex-col bg-white text-black dark:bg-slate-900 dark:text-white">
         <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <main className="padding-x flex pt-20">
+        <main className="padding-x pt-20">
           <SideBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
           {children}
         </main>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -17,11 +17,11 @@ const SideBar: React.FC<{
 
   const [focus, setFocus] = useState(false);
 
-  const searchfor: 'pokemon' | 'berry' =
-    router.pathname === '/' || '/pokemon/[pokemonName]'
+  const searchfor: 'pokemon' | 'Berries' =
+    router.pathname === '/' || router.pathname === '/pokemon/[pokemonName]'
       ? 'pokemon'
       : router.pathname === '/berry'
-      ? 'berry'
+      ? 'Berries'
       : 'pokemon';
 
   useEffect(() => {
@@ -76,7 +76,7 @@ const SideBar: React.FC<{
     <div
       className={`${
         !menuOpen && 'hidden'
-      } fixed z-40 h-full w-full flex-col overflow-y-auto max-md:top-0 max-md:left-0 max-md:bg-sky-200 max-md:px-[4vw] max-md:pt-20 dark:max-md:bg-slate-900 md:fixed md:flex md:w-1/6`}
+      } fixed z-40 flex h-full w-full flex-col gap-5 overflow-y-auto max-md:top-0 max-md:left-0 max-md:bg-sky-200 max-md:px-[4vw] max-md:pt-20 dark:max-md:bg-slate-900 md:w-1/6`}
     >
       <SearchBar
         _search={_search}
@@ -87,42 +87,57 @@ const SideBar: React.FC<{
       />
       {focus ? (
         <div>
-          <div>{searchfor}</div>
-          {searchResults.length !== 0 &&
-            focus &&
-            searchResults.map((result, index) => {
-              return index === searchIndex ? (
-                <div
-                  key={result}
-                  className="flex h-9 cursor-pointer items-center px-8"
-                  onClick={() => _search(result)}
-                >
-                  {result}
-                </div>
-              ) : (
-                <div
-                  key={result}
-                  className="flex h-9 cursor-pointer items-center px-8"
-                  onClick={() => _search(result)}
-                >
-                  {result}
-                </div>
-              );
-            })}
+          <div className="flex h-9 items-center rounded-md border-2 border-sky-200 bg-sky-100 px-3 font-bold dark:border-sky-700 dark:bg-sky-900">
+            {searchfor === 'pokemon' ? 'Pokémon' : searchfor}
+          </div>
+          <div className="mt-3 ml-3">
+            {searchResults.length !== 0 &&
+              focus &&
+              searchResults.map((result, index) => {
+                return index === searchIndex ? (
+                  <div
+                    key={result}
+                    className="flex h-9  cursor-pointer items-center gap-5 border-l-2 border-sky-300 bg-slate-100 px-5 dark:border-sky-700 dark:bg-slate-800"
+                    onClick={() => _search(result)}
+                  >
+                    <span>•</span>
+                    <span>{result}</span>
+                  </div>
+                ) : (
+                  <div
+                    key={result}
+                    className="flex h-9 cursor-pointer items-center gap-5 border-l-2 px-5 hover:bg-slate-100 dark:hover:bg-slate-800"
+                    onClick={() => _search(result)}
+                  >
+                    <span>•</span>
+                    <span>{result}</span>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       ) : (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <Link
             href={'/'}
-            className={`${router.pathname === '/' ? 'font-bold' : ''}`}
+            className={`${
+              router.pathname === '/' ||
+              router.pathname === '/pokemon/[pokemonName]'
+                ? 'border-sky-200 bg-sky-100 font-bold dark:border-sky-700 dark:bg-sky-900'
+                : 'bg-slate-50 dark:bg-slate-800'
+            } flex h-9 items-center rounded-md border-2  px-3`}
           >
             Pokémon
           </Link>
           <Link
             href={'/berry'}
-            className={`${router.pathname === '/berry' ? 'font-bold' : ''}`}
+            className={`${
+              router.pathname === '/berry'
+                ? 'border-sky-200 bg-sky-100 font-bold dark:border-sky-700 dark:bg-sky-900'
+                : 'bg-slate-50 dark:border-slate-700 dark:bg-slate-800'
+            } flex h-9 items-center rounded-md border-2 px-3`}
           >
-            Berry
+            Berries
           </Link>
         </div>
       )}
