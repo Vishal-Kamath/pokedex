e1f5ad3cf8b5ae2dfb31f0770fde63ed2a31f534
Vishal-Kamath <vishalkamath853@gmail.com>
2023-04-30 20:23:53 +0530

    ⬆️update: sidebar visibility fix

--- src/components/sidebar/searchbar.tsx
+++ src/components/sidebar/searchbar.tsx
@@ -3,7 +3,7 @@ import { FaSearch } from 'react-icons/fa';
 
 const SearchBar: React.FC = () => {
   return (
-    <div className="flex h-9 w-full items-center gap-2 overflow-hidden rounded-full border-2 border-slate-300 px-2 dark:border-slate-700">
+    <div className="flex h-9 w-full items-center gap-2 overflow-hidden rounded-full border-2 border-slate-700 px-2 dark:border-slate-300 sm:border-slate-300 sm:dark:border-slate-700">
       <FaSearch className="h-4 w-4 text-sky-600" />
       <input
         type="text"
