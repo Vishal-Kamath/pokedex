a29cfbc86c19f034b32cfaa10050ea21da49b96b
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-01 18:21:36 +0530

    ⬆️update: image to svg

--- src/app/api/og/dynamic/route.tsx
+++ src/app/api/og/dynamic/route.tsx
@@ -1,14 +1,12 @@
 import { ImageResponse } from '@vercel/og';
 
+export const runtime = 'edge';
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
   const name = searchParams.get('name');
   const img = searchParams.get('img');
-
-  const endpoint = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
 
   return new ImageResponse(
     (
@@ -22,18 +20,37 @@ export async function GET(request: Request) {
           color: 'white',
         }}
       >
-        <img
-          src={`${endpoint}/og_background.png`}
-          alt="bg"
+        {/* Background */}
+        <svg
           style={{
             position: 'absolute',
             top: '0',
             left: '0',
             zIndex: '-10',
           }}
-          width={1200}
-          height={630}
-        />
+          width="1200"
+          height="630"
+          viewBox="0 0 1200 630"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="1200" height="630" fill="url(#paint0_linear_107_2)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_107_2"
+              x1="1200"
+              y1="0.000136239"
+              x2="600"
+              y2="630"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#020617" />
+              <stop offset="0.494491" stop-color="#082F49" />
+              <stop offset="1" stop-color="#020617" />
+            </linearGradient>
+          </defs>
+        </svg>
+
         <img
           src={`${img}`}
           alt={`${name}`}
@@ -54,12 +71,35 @@ export async function GET(request: Request) {
           }}
         >
           <div style={{ display: 'flex', gap: '10px' }}>
-            <img
-              src={`${endpoint}/pokeball.png`}
-              alt="pokeball"
-              width={80}
-              height={80}
-            />
+            {/* Pokeball */}
+            <svg
+              width="80"
+              height="80"
+              viewBox="0 0 80 80"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="40" cy="40" r="12.8" fill="white" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M53.4789 29.9703L70.1617 20.3385C63.7358 10.501 52.6271 4 40 4C20.1177 4 4 20.1177 4 40C4 45.8637 5.40188 51.3999 7.88876 56.2918L24.5718 46.6599C23.6891 44.6179 23.2 42.3661 23.2 40C23.2 30.7216 30.7216 23.2 40 23.2C45.5189 23.2 50.4163 25.8612 53.4789 29.9703Z"
+                fill="#ED1B24"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M40 80C62.0914 80 80 62.0914 80 40C80 17.9086 62.0914 0 40 0C17.9086 0 0 17.9086 0 40C0 62.0914 17.9086 80 40 80ZM40 52.8C47.0692 52.8 52.8 47.0692 52.8 40C52.8 32.9308 47.0692 27.2 40 27.2C32.9308 27.2 27.2 32.9308 27.2 40C27.2 47.0692 32.9308 52.8 40 52.8ZM53.4789 29.9703L70.1617 20.3385C63.7358 10.501 52.6271 4 40 4C20.1177 4 4 20.1177 4 40C4 45.8637 5.40188 51.3999 7.88876 56.2918L24.5718 46.6599C23.6891 44.6179 23.2 42.3661 23.2 40C23.2 30.7216 30.7216 23.2 40 23.2C45.5189 23.2 50.4163 25.8612 53.4789 29.9703ZM56.8 40C56.8 49.2784 49.2784 56.8 40 56.8C34.5353 56.8 29.6799 54.1908 26.6118 50.1503L9.92 59.7873C16.3547 69.5747 27.4342 76.0362 40.0229 76.0362C59.9052 76.0362 76.0229 59.9184 76.0229 40.0362C76.0229 34.2107 74.6392 28.7084 72.1826 23.84L55.4875 33.4789C56.3327 35.484 56.8 37.6874 56.8 40Z"
+                fill="#333333"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M40 56.8001C49.2784 56.8001 56.8 49.2785 56.8 40.0001C56.8 37.6875 56.3327 35.4841 55.4875 33.479L72.1826 23.8401C74.6392 28.7085 76.0229 34.2108 76.0229 40.0363C76.0229 59.9185 59.9051 76.0363 40.0229 76.0363C27.4342 76.0363 16.3547 69.5748 9.91998 59.7874L26.6118 50.1504C29.6799 54.1909 34.5353 56.8001 40 56.8001Z"
+                fill="white"
+              />
+            </svg>
+
             <span style={{ fontSize: '70px' }}>pokédex</span>
           </div>
           <span
--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,10 +1,8 @@
 import { ImageResponse } from '@vercel/og';
 
-export async function GET() {
-  const endpoint = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
+export const runtime = 'edge';
 
+export async function GET() {
   return new ImageResponse(
     (
       <div
@@ -14,13 +12,31 @@ export async function GET() {
           color: 'white',
         }}
       >
-        <img
-          src={`${endpoint}/og_background.png`}
-          className="absolute left-0 top-0"
-          alt="bg"
-          width={1200}
-          height={630}
-        />
+        {/* Background */}
+        <svg
+          width="1200"
+          height="630"
+          viewBox="0 0 1200 630"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="1200" height="630" fill="url(#paint0_linear_107_2)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_107_2"
+              x1="1200"
+              y1="0.000136239"
+              x2="600"
+              y2="630"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#020617" />
+              <stop offset="0.494491" stop-color="#082F49" />
+              <stop offset="1" stop-color="#020617" />
+            </linearGradient>
+          </defs>
+        </svg>
+
         <div
           style={{
             position: 'absolute',
@@ -34,12 +50,35 @@ export async function GET() {
             gap: '50px',
           }}
         >
-          <img
-            src={`${endpoint}/pokeball.png`}
-            alt="pokeball"
-            width={150}
-            height={150}
-          />
+          {/* Pokeball */}
+          <svg
+            width="150"
+            height="150"
+            viewBox="0 0 150 150"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="75" cy="75" r="24" fill="white" />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M100.273 56.1944L131.553 38.1348C119.505 19.6894 98.6758 7.5 75 7.5C37.7208 7.5 7.5 37.7208 7.5 75C7.5 85.9944 10.1285 96.3748 14.7914 105.547L46.0721 87.4873C44.4171 83.6586 43.5 79.4364 43.5 75C43.5 57.603 57.603 43.5 75 43.5C85.348 43.5 94.5305 48.4897 100.273 56.1944Z"
+              fill="#ED1B24"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M75 150C116.421 150 150 116.421 150 75C150 33.5786 116.421 0 75 0C33.5786 0 0 33.5786 0 75C0 116.421 33.5786 150 75 150ZM75 99C88.2548 99 99 88.2548 99 75C99 61.7452 88.2548 51 75 51C61.7452 51 51 61.7452 51 75C51 88.2548 61.7452 99 75 99ZM100.273 56.1944L131.553 38.1348C119.505 19.6894 98.6758 7.5 75 7.5C37.7208 7.5 7.5 37.7208 7.5 75C7.5 85.9944 10.1285 96.3748 14.7914 105.547L46.0721 87.4873C44.4171 83.6586 43.5 79.4364 43.5 75C43.5 57.603 57.603 43.5 75 43.5C85.348 43.5 94.5305 48.4897 100.273 56.1944ZM106.5 75C106.5 92.397 92.397 106.5 75 106.5C64.7536 106.5 55.6499 101.608 49.8972 94.0318L18.6 112.101C30.6651 130.453 51.4392 142.568 75.0429 142.568C112.322 142.568 142.543 112.347 142.543 75.0678C142.543 64.1451 139.949 53.8282 135.342 44.7L104.039 62.773C105.624 66.5324 106.5 70.6639 106.5 75Z"
+              fill="#333333"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M75 106.5C92.3969 106.5 106.5 92.3969 106.5 75C106.5 70.6639 105.624 66.5324 104.039 62.7729L135.342 44.7C139.949 53.8282 142.543 64.145 142.543 75.0678C142.543 112.347 112.322 142.568 75.0429 142.568C51.4392 142.568 30.6651 130.452 18.6 112.101L49.8972 94.0317C55.6499 101.608 64.7536 106.5 75 106.5Z"
+              fill="white"
+            />
+          </svg>
+
           <span style={{ fontSize: '120px' }}>pokédex</span>
         </div>
       </div>
