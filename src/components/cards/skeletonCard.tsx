ea831a6bc2706efd59de4ee4d696bdbfd738116a
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-02 16:48:58 +0530

    ⬆️update: skeleton card correct height

--- src/components/cards/skeletonCard.tsx
+++ src/components/cards/skeletonCard.tsx
@@ -3,9 +3,9 @@ import SkeletonComponent from '../skeletonComponent';
 
 const SkeletonCard: React.FC = () => {
   return (
-    <div className="flex w-full flex-col gap-2 rounded-md border-2 border-slate-200 bg-slate-100 bg-opacity-40 p-2 pb-4 dark:border-slate-800 dark:bg-slate-900 dark:bg-opacity-40">
+    <div className="flex w-full flex-col gap-2 rounded-md border-2 border-slate-200 bg-slate-100 bg-opacity-40 p-2 pb-3 dark:border-slate-800 dark:bg-slate-900 dark:bg-opacity-40">
       <SkeletonComponent className="aspect-square" />
-      <SkeletonComponent />
+      <SkeletonComponent className="h-4" />
     </div>
   );
 };
