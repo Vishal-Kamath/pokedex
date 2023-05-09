c34bee6d5ade8d053261eb535e27d6095c5ae9fd
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-09 15:55:14 +0530

    ⬆️update: item link

--- src/components/pokemon/itemsHeldByPokemon.tsx
+++ src/components/pokemon/itemsHeldByPokemon.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { PokemonHeldItem } from 'pokenode-ts';
 import React from 'react';
 
@@ -10,16 +11,18 @@ const ItemsHeldByPokemon: React.FC<{ items: PokemonHeldItem[] }> = ({
       <h2 className="text-2xl">Items held by pokemon</h2>
       <div className="flex flex-col items-center justify-evenly rounded-md bg-slate-100 py-3 dark:bg-slate-800 lg:flex-row">
         {items.map((item) => (
-          <Image
-            key={item.item.name}
-            title={item.item.name}
-            alt={item.item.name}
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item.item.name}.png`}
-            className="pixel-image  aspect-square w-full md:w-[10rem]"
-            loading="lazy"
-            width="500"
-            height="500"
-          />
+          <Link href={`/items/${item.item.name}`}>
+            <Image
+              key={item.item.name}
+              title={item.item.name}
+              alt={item.item.name}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item.item.name}.png`}
+              className="pixel-image  aspect-square w-full md:w-[10rem]"
+              loading="lazy"
+              width="500"
+              height="500"
+            />
+          </Link>
         ))}
       </div>
     </div>
