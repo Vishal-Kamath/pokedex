407a3b6fa3a68a9ff823b6c59ceafec2bb3d47e5
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-11 16:14:16 +0530

    ⬆️update: migrate favicons to favicon folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,19 +31,19 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
-    apple: [{ url: '/apple-touch-icon.png', type: 'image/png' }],
+    apple: [{ url: '/favicon/apple-touch-icon.png', type: 'image/png' }],
     other: [
       {
-        url: '/android-chrome-192x192.png',
+        url: '/favicon/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        url: '/android-chrome-512x512.png',
+        url: '/favicon/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
