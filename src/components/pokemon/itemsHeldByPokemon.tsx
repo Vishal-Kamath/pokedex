3a78d61434bec6bbd26a1004e8ebe0f2733ff700
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-11 20:35:17 +0530

    🪲fix: image size

--- src/components/pokemon/itemsHeldByPokemon.tsx
+++ src/components/pokemon/itemsHeldByPokemon.tsx
@@ -11,12 +11,16 @@ const ItemsHeldByPokemon: React.FC<{ items: PokemonHeldItem[] }> = ({
       <h2 className="text-2xl">Items held by pokemon</h2>
       <div className="flex flex-col items-center justify-evenly rounded-md bg-slate-100 py-3 dark:bg-slate-800 lg:flex-row">
         {items.map((item) => (
-          <Link key={item.item.name} href={`/items/${item.item.name}`}>
+          <Link
+            key={item.item.name}
+            href={`/items/${item.item.name}`}
+            className="aspect-square w-full md:w-[10rem]"
+          >
             <Image
               title={item.item.name}
               alt={item.item.name}
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item.item.name}.png`}
-              className="pixel-image  aspect-square w-full md:w-[10rem]"
+              className="pixel-image aspect-square w-full md:w-[10rem]"
               loading="lazy"
               width="500"
               height="500"
