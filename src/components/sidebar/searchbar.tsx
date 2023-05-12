b477522c3328bf1842886d263de0793312ba4e72
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-12 18:28:44 +0530

    ✨feature: search bar blur from escape key

--- src/components/sidebar/searchbar.tsx
+++ src/components/sidebar/searchbar.tsx
@@ -61,10 +61,13 @@ const SearchBar: React.FC<{
       e.preventDefault();
       search(display);
     }
+    if (e.code === 'Escape') {
+      e.currentTarget.blur();
+    }
   };
 
   return (
-    <div className="flex h-9 w-full items-center gap-2 overflow-hidden rounded-full border-2 border-slate-700 px-2 dark:border-slate-300 sm:border-slate-300 sm:dark:border-slate-700">
+    <div className="flex h-9 w-full items-center gap-5 overflow-hidden rounded-full border-2 border-slate-700 px-2 dark:border-slate-300 sm:border-slate-300 sm:dark:border-slate-700">
       <FaSearch className="h-4 w-4 text-sky-600" />
       <input
         type="text"
