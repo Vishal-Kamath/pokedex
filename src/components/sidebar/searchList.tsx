6536cc1aa8ead6df52464dcfad0f02c32afb2a53
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-12 18:28:18 +0530

    ⬆️update: search list redesign

--- src/components/sidebar/searchList.tsx
+++ src/components/sidebar/searchList.tsx
@@ -5,6 +5,7 @@ import {
 } from '@/slice/search.slice';
 import { useAppSelector } from '@/store/hooks';
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchList: React.FC<{
   searchedFor: string;
@@ -15,30 +16,30 @@ const SearchList: React.FC<{
   const focused = useAppSelector(selectSearchFocused);
 
   return (
-    <div>
-      <div className="flex h-9 items-center rounded-md border-2 border-sky-200 bg-sky-100 px-3 font-bold dark:border-sky-700 dark:bg-sky-900">
+    <div className="flex flex-col">
+      <div className="flex h-9 items-center rounded-md border-2 border-sky-200 bg-sky-100 px-3 font-semibold capitalize dark:border-sky-700 dark:bg-sky-900">
         {searchedFor}
       </div>
-      <div className="ml-3 mt-3">
+      <div className="flex flex-col">
         {results.length !== 0 &&
           focused &&
           results.map((result, listIndex) => {
             return listIndex === index ? (
               <div
                 key={result}
-                className="flex h-9 cursor-pointer items-center gap-5 border-l-2 border-sky-300 bg-slate-100 px-5 leading-none dark:border-sky-700 dark:bg-slate-800"
+                className="flex h-9 cursor-pointer items-center gap-5 rounded-md border-sky-300 bg-slate-100 px-2 leading-none dark:border-sky-700 dark:bg-slate-800"
                 onClick={() => search(result)}
               >
-                <span>•</span>
+                <FaSearch className="text-sky-500" />
                 <span>{result}</span>
               </div>
             ) : (
               <div
                 key={result}
-                className="flex h-9 cursor-pointer items-center gap-5 border-l-2 px-5 leading-none hover:bg-slate-100 dark:hover:bg-slate-800"
+                className="flex h-9 cursor-pointer items-center gap-5 rounded-md px-2 leading-none hover:bg-slate-100 dark:hover:bg-slate-800"
                 onClick={() => search(result)}
               >
-                <span>•</span>
+                <FaSearch className="text-slate-500" />
                 <span>{result}</span>
               </div>
             );
