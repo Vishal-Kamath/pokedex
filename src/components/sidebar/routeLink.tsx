d5576ec480e4af9bfd4164448bb4ac7eb45e9b21
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-17 18:41:41 +0530

    ✨feature: clickable route links change on hover

--- src/components/sidebar/routeLink.tsx
+++ src/components/sidebar/routeLink.tsx
@@ -1,9 +1,9 @@
 import { closeSidebar } from '@/slice/sidebar.slice';
 import { useAppDispatch } from '@/store/hooks';
 import { usePathname, useRouter } from 'next/navigation';
-import React from 'react';
+import { FC } from 'react';
 
-const RouteLink: React.FC<{ title: string }> = ({ title }) => {
+const RouteLink: FC<{ title: string }> = ({ title }) => {
   const dispatch = useAppDispatch();
   const pathname = usePathname();
   const router = useRouter();
@@ -20,7 +20,7 @@ const RouteLink: React.FC<{ title: string }> = ({ title }) => {
       className={`${
         isCurrentRoute
           ? 'border-sky-500 bg-sky-400 bg-opacity-25 dark:border-sky-300 sm:border-sky-300 sm:dark:border-sky-500'
-          : 'border-slate-700 dark:border-slate-300 sm:border-slate-300 sm:dark:border-slate-700'
+          : 'border-slate-700 hover:bg-sky-300/[0.10] dark:border-slate-300 dark:hover:bg-sky-700/[0.2] sm:border-slate-300 sm:dark:border-slate-700'
       } flex h-9 cursor-pointer items-center rounded-md border-2 px-3 capitalize`}
     >
       {title}
