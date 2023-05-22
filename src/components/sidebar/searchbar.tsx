a8172e7679501f36820f1b11016d981b922c3892
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-22 18:09:30 +0530

    ⬆️update: label for search

--- src/components/sidebar/searchbar.tsx
+++ src/components/sidebar/searchbar.tsx
@@ -81,10 +81,21 @@ const SearchBar: React.FC<{
   }, []);
 
   return (
-    <div className="flex h-9 w-full items-center gap-3 overflow-hidden rounded-full border-2 border-slate-500 px-2 focus-within:border-gray-600 hover:border-sky-600 dark:border-slate-300 dark:focus-within:border-gray-400 dark:hover:border-sky-400 sm:border-slate-300 sm:dark:border-slate-700">
+    <div
+      title="you can press the S key to focus on the searchbar and the ESC key to unfocus"
+      className="flex h-9 w-full items-center gap-3 overflow-hidden rounded-full border-2 border-slate-500 px-2 focus-within:border-gray-600 hover:border-sky-600 dark:border-slate-300 dark:focus-within:border-gray-400 dark:hover:border-sky-400 sm:border-slate-300 sm:dark:border-slate-700"
+    >
+      {/* Search Icon */}
       <AiOutlineSearch className="h-5 w-5 text-sky-600" />
+
+      {/* Search Input */}
+      <label htmlFor="search_bar" aria-hidden="true" className="hidden">
+        Search Bar
+      </label>
       <input
-        tabIndex={1}
+        aria-keyshortcuts="S"
+        aria-label="Search Bar"
+        tabIndex={0}
         type="text"
         id="search_bar"
         className="h-full w-full bg-transparent outline-none"
